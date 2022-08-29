d5eeea7f9f5e4ac19fb5551b1ea39a78e10d7831
Ozan Barış GÜNAYDIN <ozangunaydin61@gmail.com>
2022-08-29 10:22:17 +0300

    Boys counting on the router and presenter

--- ToDoVIP/ToDoList/Router/ToDoListRouter.swift
+++ ToDoVIP/ToDoList/Router/ToDoListRouter.swift
@@ -12,49 +12,17 @@
 
 import UIKit
 
-@objc protocol ToDoListRoutingLogic
-{
+@objc protocol ToDoListRoutingLogic {
   //func routeToSomewhere(segue: UIStoryboardSegue?)
 }
 
-protocol ToDoListDataPassing
-{
+protocol ToDoListDataPassing {
   var dataStore: ToDoListDataStore? { get }
 }
 
-class ToDoListRouter: NSObject, ToDoListRoutingLogic, ToDoListDataPassing
-{
+class ToDoListRouter: NSObject, ToDoListRoutingLogic, ToDoListDataPassing {
   weak var viewController: ToDoListViewController?
   var dataStore: ToDoListDataStore?
-  
-  // MARK: Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
-
-  // MARK: Navigation
-  
-  //func navigateToSomewhere(source: ToDoListViewController, destination: SomewhereViewController)
-  //{
-  //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: Passing data
-  
-  //func passDataToSomewhere(source: ToDoListDataStore, destination: inout SomewhereDataStore)
-  //{
-  //  destination.name = source.name
-  //}
+    
+    // Routing logics can be added
 }
